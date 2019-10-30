f20db339935e3fd618944bed642713a98bfd4663
Tony Gallotta <anthony.gallotta@gmail.com>
2019-10-30 08:56:12 -0500

    upgrading libraries, fixing lint errors

--- App.js
+++ App.js
@@ -1,61 +1,17 @@
 import React from 'react';
-import { Platform, StatusBar, StyleSheet, View } from 'react-native';
-import { AppLoading, Asset, Font, Icon } from 'expo';
+import {
+  Platform,
+  StatusBar,
+  StyleSheet,
+  View,
+} from 'react-native';
+import {
+  AppLoading,
+  Asset,
+  Font,
+  Icon,
+} from 'expo';
 import AppNavigator from './navigation/AppNavigator';
-
-export default class App extends React.Component {
-  state = {
-    isLoadingComplete: false,
-  };
-
-  render() {
-    if (!this.state.isLoadingComplete && !this.props.skipLoadingScreen) {
-      return (
-        <AppLoading
-          startAsync={this._loadResourcesAsync}
-          onError={this._handleLoadingError}
-          onFinish={this._handleFinishLoading}
-        />
-      );
-    } else {
-      return (
-        <View style={styles.container}>
-          {Platform.OS === 'ios' && <StatusBar barStyle="default" />}
-          <AppNavigator />
-        </View>
-      );
-    }
-  }
-
-  _loadResourcesAsync = async () => {
-    return Promise.all([
-      Asset.loadAsync([
-        require('./assets/images/catpix2.png'),
-        require('./assets/images/catpix.png'),
-      ]),
-      Font.loadAsync({
-        // This is the font that we are using for our tab bar
-        ...Icon.Ionicons.font,
-        // We include SpaceMono because we use it in HomeScreen.js. Feel free
-        // to remove this if you are not using it in your app
-        'space-mono': require('./assets/fonts/SpaceMono-Regular.ttf'),
-        'zcool': require('./assets/fonts/ZCOOLQingKeHuangYou-Regular.ttf'),
-        'press-start-2p': require('./assets/fonts/PressStart2P-Regular.ttf'),
-        'jackeyfont': require('./assets/fonts/jackeyfont.ttf'),
-      }),
-    ]);
-  };
-
-  _handleLoadingError = error => {
-    // In this case, you might want to report the error to your error
-    // reporting service, for example Sentry
-    console.warn(error);
-  };
-
-  _handleFinishLoading = () => {
-    this.setState({ isLoadingComplete: true });
-  };
-}
 
 const styles = StyleSheet.create({
   container: {
@@ -63,3 +19,54 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
   },
 });
+
+async function loadResourcesAsync() {
+  return Promise.all([
+    Asset.loadAsync([
+      require('./assets/images/catpix2.png'),
+      require('./assets/images/catpix.png'),
+    ]),
+    Font.loadAsync({
+      // This is the font that we are using for our tab bar
+      ...Icon.Ionicons.font,
+      'zcool': require('./assets/fonts/ZCOOLQingKeHuangYou-Regular.ttf'),
+      'press-start-2p': require('./assets/fonts/PressStart2P-Regular.ttf'),
+      'jackeyfont': require('./assets/fonts/jackeyfont.ttf'),
+    }),
+  ]);
+}
+
+function handleLoadingError(error) {
+  // TODO: something better
+  console.warn(error);
+}
+
+export default class App extends React.Component {
+  state = {
+    isLoadingComplete: false,
+  };
+
+  handleFinishLoading() {
+    this.setState({ isLoadingComplete: true });
+  }
+
+  render() {
+    const { isLoadingComplete } = this.state;
+    const { skipLoadingScreen } = this.props;
+    if (!isLoadingComplete && !skipLoadingScreen) {
+      return (
+        <AppLoading
+          startAsync={loadResourcesAsync}
+          onError={this.handleLoadingError}
+          onFinish={this.handleFinishLoading}
+        />
+      );
+    }
+    return (
+      <View style={styles.container}>
+        {Platform.OS === 'ios' && <StatusBar barStyle="default" />}
+        <AppNavigator />
+      </View>
+    );
+  }
+}
